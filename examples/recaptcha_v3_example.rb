43de16c5706a23f70103a7743ef43a0b878fe325
victorkowalski <vkowalski@yandex.ru>
2025-11-19 16:41:56 +0300

    prosopo upd

--- examples/recaptcha_v3_example.rb
+++ examples/recaptcha_v3_example.rb
@@ -1,13 +1,15 @@
-
 require_relative '../lib/api_2captcha'
 
 client =  Api2Captcha.new(ARGV[0])
 
 begin
-result = client.prosopo({
-  pageurl: "https://www.twickets.live/",
-  sitekey: "5EZVvsHMrKCFKp5NYNoTyDjTjetoVo1Z4UNNbTwJf1GfN6Xm",
-})
+result = client.recaptcha_v3({
+                               googlekey: "6LfD3PIbAAAAAJs_eEHvoOl75_83eXSqpPSRFJ_u",
+                               pageurl: "http://2captcha.com/demo/recaptcha-v3",
+                               version: "v3",
+                               score: 0.3,
+                               action: "verify"
+                             })
 rescue Api2Captcha::ValidationException => e
     # invalid parameters passed
     puts(e)
