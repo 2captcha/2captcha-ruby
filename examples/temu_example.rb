10a616ab46f96bad21077c4ca842cea8b0b01628
victorkowalski <vkowalski@yandex.ru>
2025-12-10 13:40:59 +0300

    temu base64 upd

--- examples/temu_example.rb
+++ examples/temu_example.rb
@@ -3,17 +3,22 @@ require_relative '../lib/api_2captcha'
 
 client =  Api2Captcha.new(ARGV[0])
 
-body = File.expand_path("../media/temu/body.png", FILE)
-part1 = File.expand_path("../media/temu/part1.png", FILE)
-part2 = File.expand_path("../media/temu/part2.png", FILE)
-part3 = File.expand_path("../media/temu/part3.png", FILE)
+bodyFile = File.expand_path("../media/temu/body.png", __FILE__)
+part1File = File.expand_path("../media/temu/part1.png", __FILE__)
+part2File = File.expand_path("../media/temu/part2.png", __FILE__)
+part3File = File.expand_path("../media/temu/part3.png", __FILE__)
+
+bodyStr = Base64.strict_encode64(File.binread(bodyFile))
+part1Str = Base64.strict_encode64(File.binread(part1File))
+part2Str = Base64.strict_encode64(File.binread(part2File))
+part3Str = Base64.strict_encode64(File.binread(part3File))
 
 begin
 result = client.temu({
-  body: body,
-  part1: part1,
-  part2: part2,
-  part3: part3,
+  body: bodyStr,
+  part1: part1Str,
+  part2: part2Str,
+  part3: part3Str,
 })
 rescue StandardError => e
     puts(e)
