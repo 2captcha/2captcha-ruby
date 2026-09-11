2943173dc6e2a3b3d62243d1b38219a3e863f2d1
victorkowalski <vkowalski@yandex.ru>
2026-09-11 18:16:00 +0300

    uri

--- lib/api_2captcha/generic_client.rb
+++ lib/api_2captcha/generic_client.rb
@@ -9,33 +9,95 @@ require "open-uri"
 module Api2Captcha
   class GenericClient
 
-        attr_accessor :domain, :callback, :default_timeout, :recaptcha_timeout,
-                  :polling_interval, :api_key, :soft_id
+        attr_accessor :domain, :callback, :default_timeout, :recaptcha_timeout, :polling_interval, :api_key, :soft_id,
+                  :createTaskUri, :getTaskResultUri, :getBalanceUri, :reportCorrectUri, :reportIncorrectUri
+
+
+
+
 
     def initialize(api_key)
       @api_key = api_key
-      @soft_id = soft_id
       @callback = callback
       @default_timeout = 120
       @recaptcha_timeout = 600
       @polling_interval = 10
       @soft_id = 4584
-      @domain = DEFAULT_DOMAIN
+      @createTaskUri = "https://api.rucaptcha.com/createTask"
+      @getTaskResultUri = "https://api.rucaptcha.com/getTaskResult"
+      @getBalanceUri = "https://api.rucaptcha.com/getBalance"
+      @reportCorrectUri = "https://api.rucaptcha.com/reportCorrect"
+      @reportIncorrectUri = "https://api.rucaptcha.com/reportIncorrect"
     end
 
     def solve(data)
       data[:softId] = soft_id
+       response = createTask(data)
+      #JSONObject responseJsonObject = createTask(jsonObject);
+      #if (jsonObject.getJSONObject("task").has("callbackUrl")
+      #          && !jsonObject.getJSONObject("task").getString("callbackUrl").isEmpty())
+      #      return responseJsonObject;
+      #  return getTaskResult(this.taskId);
+
     end
+
+    def createTask(data)
+        puts "CreateTask Request"
+        return doRequest(createTaskUri, data);
+    end
+
+    def doRequest(uri, data)
+      
+    end
+    
   end
 end
 
-#     public JSONObject solve(JSONObject jsonObject) throws Exception {
-#         jsonObject.put("softId", softId);
-#         JSONObject responseJsonObject = createTask(jsonObject);
-#         this.taskId = responseJsonObject.getLong("taskId");
-#
-#         if (jsonObject.getJSONObject("task").has("callbackUrl")
-#                 && !jsonObject.getJSONObject("task").getString("callbackUrl").isEmpty())
-#             return responseJsonObject;
-#         return getTaskResult(this.taskId);
-#     }
+=begin
+#         private JSONObject doRequest(String uri, JSONObject jsonObject) throws Exception {
+        HttpRequest request = request(jsonObject, uri);
+        HttpResponse<String> response = httpClient.send(request, HttpResponse.BodyHandlers.ofString());
+
+        System.out.println("Status: " + response.statusCode());
+        System.out.println("Body: " + response.body());
+
+        JSONObject responseJsonObject = new JSONObject(response.body());
+        return responseJsonObject;
+    }
+
+
+--------------------------------------------
+require 'net/http'
+require 'uri'
+require 'json'
+
+# 1. Define the URI and data
+uri = URI.parse('https://example.com')
+payload = {
+  name: 'John Doe',
+  email: 'john@example.com'
+}
+
+# 2. Set up the Net::HTTP object
+http = Net::HTTP.new(uri.host, uri.port)
+http.use_ssl = true if uri.scheme == 'https' # Enable SSL for HTTPS URLs
+
+# 3. Create the POST request
+request = Net::HTTP::Post.new(uri.request_uri)
+request['Content-Type'] = 'application/json'
+request['Accept'] = 'application/json'
+request.body = payload.to_json
+
+# 4. Send the request and handle the response
+response = http.request(request)
+
+if response.code == '200' || response.code == '201'
+  puts "Success!"
+  puts JSON.parse(response.body)
+else
+  puts "Error: #{response.code} - #{response.message}"
+end
+
+
+=
+end
