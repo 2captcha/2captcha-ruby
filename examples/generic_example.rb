a7f7f4f9963ad681cf8ed97e42d00b077f44b768
victorkowalski <vkowalski@yandex.ru>
2026-09-17 12:07:35 +0300

    reportIncorrect

--- examples/generic_example.rb
+++ examples/generic_example.rb
@@ -21,12 +21,46 @@ end
 
 puts "Result: #{result.inspect}"
 
-# get balance
+# get balance --------------------------------------------
 balance_params = {
   clientKey: key
 }
 begin
   result = client.getBalance(balance_params)
+rescue StandardError => e
+  puts "Error occurred: #{e.message}"
+end
+
+puts "Result: #{result.inspect}"
+
+# Report Correct --------------------------------------------
+
+begin
+  return if client.taskId == -1
+
+  report_correct_params = {
+    clientKey: key,
+    taskId: client.taskId
+  }
+
+  result = client.reportCorrect(report_correct_params)
+rescue StandardError => e
+  puts "Error occurred: #{e.message}"
+end
+
+puts "Result: #{result.inspect}"
+
+# Report Incorrect --------------------------------------------
+
+begin
+  return if client.taskId == -1
+
+  report_incorrect_params = {
+    clientKey: key,
+    taskId: client.taskId
+  }
+
+  result = client.reportIncorrect(report_incorrect_params)
 rescue StandardError => e
   puts "Error occurred: #{e.message}"
 end
--- lib/api_2captcha/generic_client.rb
+++ lib/api_2captcha/generic_client.rb
@@ -97,56 +97,16 @@ module Api2Captcha
         puts "Balance Request"
         return doRequest(getBalanceUri, data);
     end
+
+    def reportCorrect(data)
+        puts "Report Correct Request"
+        return doRequest(reportCorrectUri, data);
+    end
     
+    def reportIncorrect(data)
+        puts "Report Incorrect Request"
+        return doRequest(reportIncorrectUri, data);
+    end
+
   end
 end
-
-
-
-
-=begin
-#         private JSONObject doRequest(String uri, JSONObject jsonObject) throws Exception {
-        HttpRequest request = request(jsonObject, uri);
-        HttpResponse<String> response = httpClient.send(request, HttpResponse.BodyHandlers.ofString());
-
-        System.out.println("Status: " + response.statusCode());
-        System.out.println("Body: " + response.body());
-
-        JSONObject responseJsonObject = new JSONObject(response.body());
-        return responseJsonObject;
-    }
-
-
---------------------------------------------
-require 'net/http'
-require 'uri'
-require 'json'
-
-# 1. Define the URI and data
-uri = URI.parse('https://example.com')
-payload = {
-  name: 'John Doe',
-  email: 'john@example.com'
-}
-
-# 2. Set up the Net::HTTP object
-http = Net::HTTP.new(uri.host, uri.port)
-http.use_ssl = true if uri.scheme == 'https' # Enable SSL for HTTPS URLs
-
-# 3. Create the POST request
-request = Net::HTTP::Post.new(uri.request_uri)
-request['Content-Type'] = 'application/json'
-request['Accept'] = 'application/json'
-request.body = payload.to_json
-
-# 4. Send the request and handle the response
-response = http.request(request)
-
-if response.code == '200' || response.code == '201'
-  puts "Success!"
-  puts JSON.parse(response.body)
-else
-  puts "Error: #{response.code} - #{response.message}"
-end
-
-=end
